63d5abef1681aa9a248525a07baf22bbefad6bae
Kirill <kirillmartuk@gmail.com>
2021-06-28 21:53:29 +0700

    feat: add global string, like android values/strings

---
+++ lib/utils/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const String disclaimer = "Нейросеть не знает, что говорит, и может сказать всякое — если что, не обижайтесь. Распространяя получившиеся тексты, помните об ответственности.";
+  static const String agreed = "Договорились";
+  static const String inputHint = "Напишите что-нибудь и получите продолжение от Балабобы";
+  static const String makeBalabol = "Набалобобить";
+}
